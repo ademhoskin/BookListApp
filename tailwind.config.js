cf23ae74d52f4c244c8267d7dcb98ade7508f67e
Adem Hoskin <ademjhoskin@gmail.com>
2023-06-01 14:24:04 -0400

    moved files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-mode: 'jit',
+  mode: "jit",
   content: ["./src/**/*.{html,js}"],
   purge: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
-        'helve': ['"Helvetica Neue"', 'sans-serif']
-      }
+        helve: ['"Helvetica Neue"', "sans-serif"],
       },
+    },
   },
   plugins: [],
-}
+};
