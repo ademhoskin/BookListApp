0fc4a2ac9db6b26fd7e0f21fa4bc41a94d5f46db
Adem Hoskin <ademjhoskin@gmail.com>
2023-06-01 15:58:54 -0400

    completed viewlist functionality

--- src/script.js
+++ src/script.js
@@ -25,27 +25,57 @@ addBtn.addEventListener("click", () => {
 });
 // create an eventlistener for the showBooksBtn that on click, will display the books in the myLibrary array in the bookList modal window
 showBooksBtn.addEventListener("click", () => {
-  let booksContainer = document.getElementById("booksContainer");
-
-  for (i = 0; i < myLibrary.length; i++) {
+  while (booksContainer.firstChild) {
+    booksContainer.firstChild.remove();
+  }
+  for (let i = 0; i < myLibrary.length; i++) {
     const book = document.createElement("div");
     book.classList.add("bg-white", "p-2", "rounded", "shadow", "mb-2");
 
     const title = document.createElement("h3");
-    title.classList.add("text-2xl", "font-bold");
+    title.classList.add("text-2xl", "font-bold", "text-black");
     title.textContent = myLibrary[i].title;
 
     const author = document.createElement("p");
-    author.classList.add("text-lg", "font-semibold");
+    author.classList.add("text-lg", "font-semibold", "text-black");
     author.textContent = myLibrary[i].author;
 
     const genre = document.createElement("p");
-    genre.classList.add("text-lg", "font-semibold");
+    genre.classList.add("text-lg", "font-semibold", "text-black");
     genre.textContent = myLibrary[i].genre;
+
+    const readLabel = document.createElement("label");
+    readLabel.classList.add("text-lg", "font-semibold", "text-black");
+    readLabel.textContent = "Read: ";
+
+    const readCheckbox = document.createElement("input");
+    readCheckbox.classList.add(
+      "checkbox",
+      "checkbox-sm",
+      "mr-2",
+      "border-black"
+    );
+    readCheckbox.type = "checkbox";
+    readCheckbox.checked = myLibrary[i].read;
+    readCheckbox.addEventListener("change", () => {
+      myLibrary[i].read = readCheckbox.checked;
+    });
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("btn", "btn-error", "btn-sm", "ml-64");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.addEventListener("click", () => {
+      myLibrary.splice(i, 1);
+      book.remove();
+    });
+
+    readLabel.appendChild(readCheckbox);
 
     book.appendChild(title);
     book.appendChild(author);
     book.appendChild(genre);
+    book.appendChild(readLabel);
+    book.appendChild(deleteBtn);
 
     booksContainer.appendChild(book);
   }
