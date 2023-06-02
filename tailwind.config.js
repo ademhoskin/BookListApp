453626ee221e461b14f2d998cef8f0916d6df117
Adem Hoskin <ademjhoskin@gmail.com>
2023-06-01 20:26:43 -0400

    moved index to main directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   mode: "jit",
   content: ["./src/**/*.{html,js}", "./index.html"],
-  purge: ["./src/**/*.{html,js}, "./index.html"],
+  purge: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
     extend: {
       fontFamily: {
